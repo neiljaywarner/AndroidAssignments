0aeb29db11f8ffe03fe73027f35cf79dd31ebad6
nwarner <nwarner@esi-estech.com>
2015-07-05 23:30:44 -0500

    Sample code working.

--- app/src/main/java/com/esi_estech/opengltest/MyGLRenderer.java
+++ app/src/main/java/com/esi_estech/opengltest/MyGLRenderer.java
@@ -12,6 +12,7 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
 
     private static final String TAG = MyGLRenderer.class.getSimpleName();
     private Triangle mTriangle;
+    private Square mSquare;
 
     // mMVPMatrix is an abbreviation for "Model View Projection Matrix"
     private final float[] mMVPMatrix = new float[16];
@@ -26,44 +27,60 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
         mTriangle = new Triangle();
+        mSquare = new Square();
 
     }
 
     public void onDrawFrame(GL10 unused) {
-        // Redraw background color
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        float[] scratch = new float[16];
 
 
-            float[] scratch = new float[16];
+        // Draw background color
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
 
-            // Draw background color
-            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+        // Set the camera position (View matrix)
+        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
 
+        // Calculate the projection and view transformation
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
 
-            // Create a rotation for the triangle
+        // Draw square
+        mSquare.draw(mMVPMatrix); //also gets coors for triangles?
+        // Create a rotation for the triangle
 
-            // Use the following code to generate constant rotation.
-            // Leave this code out when using TouchEvents.
-            // long time = SystemClock.uptimeMillis() % 4000L;
-            // float angle = 0.090f * ((int) time);
+        // Use the following code to generate constant rotation.
+        // Leave this code out when using TouchEvents.
+        // long time = SystemClock.uptimeMillis() % 4000L;
+        // float angle = 0.090f * ((int) time);
 
-            Matrix.setRotateM(mRotationMatrix, 0, mAngle, 0, 0, 1.0f);
+        Matrix.setRotateM(mRotationMatrix, 0, mAngle, 0, 0, 1.0f);
 
-            // Combine the rotation matrix with the projection and camera view
-            // Note that the mMVPMatrix factor *must be first* in order
-            // for the matrix multiplication product to be correct.
-            Matrix.multiplyMM(scratch, 0, mMVPMatrix, 0, mRotationMatrix, 0);
+        // Combine the rotation matrix with the projection and camera view
+        // Note that the mMVPMatrix factor *must be first* in order
+        // for the matrix multiplication product to be correct.
+        Matrix.multiplyMM(scratch, 0, mMVPMatrix, 0, mRotationMatrix, 0);
 
-            // Draw triangle
-            mTriangle.draw(scratch);
+        // Draw triangle
+        mTriangle.draw(scratch);
     }
 
 
 
 
+    @Override
     public void onSurfaceChanged(GL10 unused, int width, int height) {
+        // Adjust the viewport based on geometry changes,
+        // such as screen rotation
         GLES20.glViewport(0, 0, width, height);
+
+        float ratio = (float) width / height;
+
+        // this projection matrix is applied to object coordinates
+        // in the onDrawFrame() method
+        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
+
     }
+
 
 
     public static int loadShader(int type, String shaderCode){
